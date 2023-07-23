81ff05520911731d014fac0582b9bfecef1aba62
castilloxavie <xavieralbertocastillovaron@hotmail.com>
2023-07-23 15:56:17 -0500

    crud en js

---
+++ service/client-service.js
@@ -0,0 +1,61 @@
+// Abrir el http(metodo, url)
+// CRUD = Metodos http
+//Create= POST, Read=GET, Update=PUT/PATH, Delete=DELETE
+
+
+//fatch API
+const listaClientes =  () => fetch('http://localhost:3000/perfil').then( respuesta => respuesta.json());
+
+function crearCliente(nombre, email) {
+    return fetch('http://localhost:3000/perfil', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nombre,
+            email,
+            id: uuid.v4()
+        })
+    })
+};
+
+const eliminarCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+        method: "DELETE"
+    });
+}
+
+const detalleCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`).then( (respuesta) => respuesta.json());
+}
+
+const actualizarCliente = (nombre, email, id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            nombre, 
+            email})
+    })
+        .then(respuesta => respuesta)
+        .catch((err) => console.log(err))
+}
+
+export const clientServices = {
+    listaClientes,
+    crearCliente,
+    eliminarCliente,
+    detalleCliente,
+    actualizarCliente
+};
+
+
+
+
+
+
+
+
